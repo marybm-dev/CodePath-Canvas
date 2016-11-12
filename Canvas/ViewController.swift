4d7a2f4ef3cd34680eaaab47e4a24182ba2621af
Mary Martinez <mlmartinez85@gmail.com>
2016-11-12 10:13:08 -0800

    Add animation

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -14,21 +14,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var trayView: UIView!
 
     var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        trayCenterWhenOpen = CGPoint(x: trayView.center.x, y: 550 )
+        trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: 540 + 210)
     }
     
     @IBAction func onTrayPanGesture(_ panGestureRecognizer: UIPanGestureRecognizer) {
-        let translation = panGestureRecognizer.translation(in: parentView)
+        let velocity = panGestureRecognizer.velocity(in: parentView)
         
         if panGestureRecognizer.state == .began {
            trayOriginalCenter = trayView.center
             
         } else if panGestureRecognizer.state == .changed {
-            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
             
+            if velocity.y > 0 { // moving down
+                UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+                    self.trayView.center.y = self.trayCenterWhenClosed.y
+                })
+                
+            } else { // moving up
+                UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+                    self.trayView.center.y = self.trayCenterWhenOpen.y
+                })
+            }
+
         } else if panGestureRecognizer.state == .ended {
             
         }
